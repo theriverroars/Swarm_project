e65c71c0e8d217c1f71b43757a1799d5fa46f04a
Rajpal <90927685+Rajpal9@users.noreply.github.com>
2023-04-04 10:27:14 +0530

    Added Quaternion decompression and thrust to pwm

--- utility_functions.py
+++ utility_functions.py
@@ -5,6 +5,8 @@ Created on Tue Apr 12 14:50:16 2022
 @author: My PC
 """
 import math
+from scipy.spatial.transform import Rotation as R
+
 
 def point_wrt_circle(x, y, c_x, c_y, r):
     return pow((x - c_x), 2) + pow((y - c_y), 2) - pow(r,2)
@@ -34,3 +36,50 @@ def matrix_multiplication(A, B):
                         for B_col in zip(*B)]
                                 for A_row in A]
     return result
+    
+## decompress quaternion data from stateZ
+def decompressquat(comp):
+    comp = np.uint32(comp)
+    q = np.array([0., 0., 0., 0.], dtype=np.float32)
+    
+    M_SQRT1_2 = float(1/np.sqrt(2))
+    mask = (1 << 9) - 1
+    i_largest = comp >> 30
+    sum_squares = 0.0
+    
+    for i in range(3, -1, -1):
+        if i != i_largest:
+            mag = comp & mask
+            negbit = (comp >> 9) & 0x1
+            comp = comp >> 10
+            q[i] = M_SQRT1_2 * (float(mag))/mask
+            if negbit == 1:
+                q[i] = -q[i]
+     
+            sum_squares += q[i] * q[i]
+            
+    q[i_largest] = np.sqrt(1.0 - sum_squares)
+    return q
+    
+    
+## convert quaternion to euler
+
+def comp_quat_to_euler(comp_quat):
+    q = decompressquat(comp_quat)
+    r = R.from_quat([q[0], q[1], q[2],q[3]])
+    s= r.as_euler('xyz', degrees=False)
+    return s[0],s[1],s[2]
+
+## convert thrust
+def convert_thrust_2_pwm(thrust):
+    # Refer http://mikehamer.info/assets/papers/Crazyflie%20Modelling.pdf
+    # thrust = = 2.130295e-11*cmd^2 + 1.032633e-6*cmd + 5.484560e-4
+    a = 2.130295e-11
+    b = 1.032633e-6
+    c = 5.484560e-4
+    pwm = np.roots([a, b, c - thrust])
+    result = 0
+    for val in pwm:
+        if not np.iscomplex(val) and val > 0:
+            result = val
+    return int(result)
